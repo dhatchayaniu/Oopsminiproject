b2c46c92a1f6dcd3352b18eb0564af3d10ef6e4f
Dhatchayani U <dhatchayani.240066@aids.ritchennai.edu.in>
2025-10-30 19:45:23 +0530

    Create Customer.java

---
+++ Customer.java
@@ -0,0 +1,14 @@
+package model;
+public class Customer {
+    private String customerId;
+    private String name;
+    private String contact;
+    public Customer(String customerId, String name, String contact) {
+        this.customerId = customerId;
+        this.name = name;
+        this.contact = contact;
+    }
+    public String getCustomerId() { return customerId; }
+    public String getName() { return name; }
+    public String getContact() { return contact; }
+}
